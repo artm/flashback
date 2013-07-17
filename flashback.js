0272dced2aeeefe0d6f1adf2b1b89764284c07d0
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-07-17 19:13:23 +0200

    iterate over cards array

--- flashback.js
+++ flashback.js
@@ -1,28 +1,47 @@
-//
 $(function() {
-  var card = $(".card");
+  var cards = [
+    { front: "foo", back: "FOO" },
+    { front: "bar", back: "BAR" },
+    { front: "baz", back: "BAZ" },
+    { front: "qux", back: "QUX" },
+  ];
+  var cardIndex = 0;
+  var cardView = $(".card");
+  var frontView = cardView.find(".front");
+  var backView = cardView.find(".back");
+
+  var card = function() { return cards[cardIndex]; }
+
+  var refreshCardView = function() {
+    frontView.text( card().front );
+    backView.text( card().back );
+    cardView.css("top",0);
+    cardView.css("left", - cardView.outerWidth());
+    cardView.animate({left: 0, opacity: 1.0});
+  }
 
   var nextCard = function() {
-    card.css("top",0);
-    card.css("left", - card.outerWidth());
-    card.animate({left: 0, opacity: 1.0});
+    cardIndex++;
+    if (cardIndex < cards.length) {
+      refreshCardView();
+    }
   };
 
   var reschedule = function(when) {
-    var offset = ((when==="soon") ? "-" : "+") + card.outerHeight();
-    card.animate({top: offset, opacity: 0}, nextCard);
+    var offset = ((when==="soon") ? "-" : "+") + cardView.outerHeight();
+    cardView.animate({top: offset, opacity: 0}, nextCard);
   };
   var cardUp = function() {
-    if (card.hasClass("flipped")) {
+    if (cardView.hasClass("flipped")) {
       reschedule("soon");
     }
-    card.toggleClass("flipped");
+    cardView.toggleClass("flipped");
   };
   var cardDown = function() {
-    if (card.hasClass("flipped")) {
+    if (cardView.hasClass("flipped")) {
       reschedule("later");
     }
-    card.toggleClass("flipped");
+    cardView.toggleClass("flipped");
   };
   var cardRight = function() {
   };
@@ -37,4 +56,6 @@ $(function() {
       case 37: cardLeft(); break;
     }
   });
+
+  refreshCardView();
 });
