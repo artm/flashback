2827591318001b92ff0bbf44cfb1a3e3ea6ea419
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-07-27 15:02:16 +0200

    active page link in navbar

--- lib/service/webapp.rb
+++ lib/service/webapp.rb
@@ -5,6 +5,6 @@ get("/flashback") { redirect to "/flashback/drill" }
 get("/flashback/") { redirect to "/flashback/drill" }
 Dir["views/*.slim"].each do |file|
   basename = File.basename file, ".slim"
-  get("/flashback/#{basename}") { slim basename.to_sym, locals: { page_script: basename } }
+  get("/flashback/#{basename}") { slim basename.to_sym, locals: { page: basename } }
 end
 get("/flashback/:file") { send_file File.join settings.public_folder, params[:file] }
